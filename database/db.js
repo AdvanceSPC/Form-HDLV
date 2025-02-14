161e2df7045dffe63a75cc8752b2edd53d621085
Layedra75 <layedra75@gmail.com>
2025-02-14 09:20:02 -0500

    se configuro nuevamente el vercel.json

--- database/db.js
+++ database/db.js
@@ -9,8 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 10,
-  connectTimeout: 60000,
-  acquireTimeout: 60000,
+  connectTimeout: 60000,            // Este timeout sí es válido
   waitForConnections: true,
   queueLimit: 0,
   enableKeepAlive: true,
